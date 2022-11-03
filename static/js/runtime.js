9bff4881af97096abd42a2376b3e19b615143ddd
illusioneeer <illusioneeer@gmail.com>
2022-11-03 23:03:04 +0800

    Site updated: 2022-11-03 23:03:03

--- static/js/runtime.js
+++ static/js/runtime.js
@@ -1,1 +1,36 @@
-var now=new Date;function createtime(){now.setTime(now.getTime()+1e3);var e=new Date("08/01/2022 00:00:00"),t=Math.trunc(234e8+(now-e)/1e3*17),a=(t/1496e5).toFixed(6),n=new Date("08/09/2022 00:00:00"),s=(now-n)/1e3/60/60/24,o=Math.floor(s),r=(now-n)/1e3/60/60-24*o,i=Math.floor(r);1==String(i).length&&(i="0"+i);var l=(now-n)/1e3/60-1440*o-60*i,b=Math.floor(l);1==String(b).length&&(b="0"+b);var d=(now-n)/1e3-86400*o-3600*i-60*b,c=Math.round(d);1==String(c).length&&(c="0"+c);let g="";g=i<18&&i>=9?`<img class="boardsign" src="https://img.shields.io/badge/F小屋-上班摸鱼中-6adea8?style=social&logo=cakephp" title="距离月入25k也就还差一个大佬带我~"><span class="textTip"> <br> <b>本站居然运行了 ${o} 天</b></span><span id="runtime"> ${i} 小时 ${b} 分 ${c} 秒 </span> <i id="heartbeat" class="fas fa-heartbeat" style="color:red"></i> <br> <b>旅行者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</b> <br> <b><font size="2px">再看看那个光点，它就在这里，这是家园，这是我们 ——— 你所爱的每一个人，你认识的一个人，你听说过的每一个人，曾经有过的每一个人，都在它上面度过他们的一生✨</font></b> `:`<img class="boardsign" src="https://img.shields.io/badge/F小屋-打烊休息啦-6adea8?style=social&logo=coffeescript" title="下班了就该开开心心的玩耍，嘿嘿~"><span class="textTip"> <br> <b>本站居然运行了 ${o} 天</b></span><span id="runtime"> ${i} 小时 ${b} 分 ${c} 秒 </span> <i id="heartbeat" class="fas fa-heartbeat" style="color:red"></i> <br> <b>旅行者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</b> <br> <b><font size="2px">再看看那个光点，它就在这里，这是家园，这是我们 ——— 你所爱的每一个人，你认识的一个人，你听说过的每一个人，曾经有过的每一个人，都在它上面度过他们的一生✨</font></b> `,document.getElementById("workboard")&&(document.getElementById("workboard").innerHTML=g)}setInterval((()=>{createtime()}),1e3);</18&&i>
+setInterval(() => {
+    let create_time = Math.round(new Date('2022-08-09 00:00:00').getTime() / 1000); //在此行修改建站时间
+    let timestamp = Math.round((new Date().getTime()) / 1000);
+    let second = timestamp - create_time;
+    let time = new Array(0, 0, 0, 0, 0);
+
+    var nol = function(h){
+        return h>9?h:'0'+h;
+    }
+    if (second >= 365 * 24 * 3600) {
+        time[0] = parseInt(second / (365 * 24 * 3600));
+        second %= 365 * 24 * 3600;
+    }
+    if (second >= 24 * 3600) {
+        time[1] = parseInt(second / (24 * 3600));
+        second %= 24 * 3600;
+    }
+    if (second >= 3600) {
+        time[2] = nol(parseInt(second / 3600));
+        second %= 3600;
+    }
+    if (second >= 60) {
+        time[3] = nol(parseInt(second / 60));
+        second %= 60;
+    }
+    if (second > 0) {
+        time[4] = nol(second);
+    }
+    if ((Number(time[2])<22) && (number(time[2])>7)){
+        currentTimeHtml ="<img class="boardsign" src="https://img.shields.io/badge/糖果屋-营业中-6adea8?style=social&logo=cakephp" title="距离百年老店也就差不到一百年~"><div id="runtime">" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+    }
+    else{
+        currentTimeHtml ="<img class="boardsign" src="https://img.shields.io/badge/糖果屋-打烊了-6adea8?style=social&logo=coffeescript" title="这个点了应该去睡觉啦，熬夜对身体不好哦"><div id="runtime">" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+    }
+    document.getElementById("workboard").innerHTML = currentTimeHtml;
+}, 1000);</22)>
