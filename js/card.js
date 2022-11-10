74fd97a291420ecd7fbdaad3a1cfbe0aa2434e37
illusioneeer <illusioneeer@gmail.com>
2022-11-11 02:06:24 +0800

    Site updated: 2022-11-11 02:06:23

--- js/card.js
+++ js/card.js
@@ -11,6 +11,7 @@ $(function() {
         if($('.status-bar_clock') || $('.status-bar_clock').size() > 0) {
             $('.status-bar_clock').text(getDate());
             flag = true;
+            $('#aside-content .card-info .author-info__description').text('一名什锦区博主，这里我将记录学习过程中的笔记、生活中的点点滴滴，分享一些经验与想法。在这里相遇很幸运 💛💙');
             clearInterval(t);
         }
     }, 100)
